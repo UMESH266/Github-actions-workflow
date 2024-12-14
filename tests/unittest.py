54dcb3f4725681ab523e1219ce35b5b4cb7e4244
UMESH266 <144141198+UMESH266@users.noreply.github.com>
2024-12-14 17:46:32 +0530

    code and unit test case added

---
+++ tests/unittest.py
@@ -0,0 +1,14 @@
+from src.math_operations import add, substract
+
+def addition():
+    assert add(1, 1) == 2
+    assert add(-1, 1) == 0
+    assert add(-1, -1) == -2
+    assert add(2, 3) == 5
+
+def substraction():
+    assert substract(1, 1) == 0
+    assert substract(1, -1) == 2
+    assert substract(-1, -1) == 0
+    assert substract(-1, 1) == -2
+
